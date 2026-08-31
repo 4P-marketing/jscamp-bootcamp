86fea3057c334bdfe5d7efa7e4db2ed3863d487a
madeval <alvarez.fing@gmail.com>
2026-08-31 16:06:46 -0300

    refactor: unificar validaciones de middleware para simplificar el código

--- 08-ejercicio-testing-con-node-y-zod/routes/jobs.js
+++ 08-ejercicio-testing-con-node-y-zod/routes/jobs.js
@@ -4,22 +4,9 @@ import { validateJob, validatePartialJob } from '../schemas/jobs.js'
 
 export const jobsRouter = Router()
 
-function validateCreate(req, res, next) {
-  const result = validateJob(req.body)
-
-  if (!result.success) {
-    return res.status(400).json({
-      error: 'Invalid Request',
-      details: result.error.issues,
-    })
-  }
-
-  req.body = result.data
-  next()
-}
-
-function validatePartialUpdate(req, res, next) {
-  const result = validatePartialJob(req.body)
+// Con esto, evitamos tener dos middleware casi iguales y simplificamos el comportamiento de las funciones
+const validate = (validateFn) => (req, res, next) => {
+  const result = validateFn(req.body)
 
   if (!result.success) {
     return res.status(400).json({
@@ -35,10 +22,13 @@ function validatePartialUpdate(req, res, next) {
 jobsRouter.get('/', JobController.getAll)
 jobsRouter.get('/:id', JobController.getId)
 
-jobsRouter.post('/', validateCreate, JobController.create)
+// jobsRouter.post('/', validateCreate, JobController.create)
+jobsRouter.post('/', validate(validateJob), JobController.create)
 
-jobsRouter.put('/:id', validateCreate, JobController.update)
+// jobsRouter.put('/:id', validateCreate, JobController.update)
+jobsRouter.put('/:id', validate(validateJob), JobController.update)
 
-jobsRouter.patch('/:id', validatePartialUpdate, JobController.partialUpdate)
+// jobsRouter.patch('/:id', validatePartialUpdate, JobController.partialUpdate)
+jobsRouter.patch('/:id', validate(validatePartialJob), JobController.partialUpdate)
 
 jobsRouter.delete('/:id', JobController.delete)
